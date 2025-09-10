6c16bba67470cd8e235110ea1dc472203a0deafe
unknown <pedrocunha.pessoamoema@gmail.comgit config --global user.name>
2025-09-10 00:46:46 -0300

    remova Aula 1.ts

--- Itens aleatorios/numeroaleatorio.ts
+++
@@ -1,6 +0,0 @@
-function gerarNumeroAleatorio(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const numero = gerarNumeroAleatorio(1, 100);
-console.log(`O número aleatório gerado foi: ${numero}`);
