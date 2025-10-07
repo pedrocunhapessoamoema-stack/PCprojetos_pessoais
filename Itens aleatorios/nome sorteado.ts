2ebc29e1bbc4f7a8fb9202f8717b11a3baa0c30e
Pedro <pedrocunha.pessoamoema@gmail.com>
2025-10-06 23:19:19 -0300

    Início projeto prompts

--- Itens aleatorios/nome sorteado.ts
+++
@@ -1,9 +0,0 @@
-const nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"];
-
-function sortearNome(lista: string[]): string {
-    const indice = Math.floor(Math.random() * lista.length);
-    return lista[indice];
-}
-
-const nomeSorteado = sortearNome(nomes);
-console.log(`O nome sorteado foi: ${nomeSorteado}`);
--- README.md
+++
@@ -1,2 +0,0 @@
-# Trabalho-Tutores-Computa-o-em-Nuvem
-Projetos realizado no curso!
